8fe4a36aba01cd85bb602edd170b71a112133958
andrijaristic <andra.rs@outlook.com>
2023-05-15 14:44:27 +0200

    Added cart reducer

---
+++ client/src/store/cartSlice.js
@@ -0,0 +1,110 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items:
+    localStorage.getItem("cart") !== null
+      ? JSON.parse(localStorage.getItem("cart"))
+      : null,
+  amount:
+    localStorage.getItem("cartAmount") !== null
+      ? JSON.parse(localStorage.getItem("cartAmount"))
+      : null,
+  price:
+    localStorage.getItem("cartPrice") !== null
+      ? JSON.parse(localStorage.getItem("cartPrice"))
+      : null,
+};
+
+const storeCart = (state) => {
+  localStorage.setItem("cart", JSON.stringify(state.items));
+  localStorage.setItem("cartAmount", state.amount);
+  localStorage.setItem("cartPrice", state.price);
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const item = state.items.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.amount++;
+      } else {
+        const newItem = {
+          id: action.payload.id,
+          item: { ...action.payload },
+          amount: 1,
+        };
+
+        state.items.push(newItem);
+      }
+
+      state.amount++;
+      state.price += action.payload.price;
+
+      storeCart(state);
+    },
+    removeFromCart(state, action) {
+      const item = state.items.find((item) => item.id === action.payload.id);
+      if (!item) {
+        return;
+      }
+
+      state.items = state.items.filter((item) => item.id !== action.payload.id);
+      state.amount -= item.amount;
+      state.price -= item.amount * action.payload.price;
+
+      storeCart(state);
+    },
+    increase(state, action) {
+      const item = state.items.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.amount++;
+      } else {
+        const newItem = {
+          id: action.payload.id,
+          item: { ...action.payload },
+          amount: 1,
+        };
+
+        state.items.push(newItem);
+      }
+
+      state.amount++;
+      state.price += action.payload.price;
+
+      storeCart(state);
+    },
+    decrease(state, action) {
+      const item = state.items.find((item) => item.id === action.payload.id);
+      if (!item) {
+        return;
+      }
+
+      item.amount--;
+      state.amount--;
+      state.price -= action.payload.price;
+      if (item.amount === 0) {
+        // Everything but item
+        state.items = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
+      }
+
+      storeCart(state);
+    },
+    clearCart(state, action) {
+      state.items = [];
+      state.amount = 0;
+      state.price = 0;
+
+      localStorage.removeItem("cart");
+      localStorage.removeItem("cartAmount");
+      localStorage.removeItem("cartPrice");
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, increase, decrease, clearCart } =
+  cartSlice.actions;
+export default cartSlice.reducer;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import productsSlice from "./productsSlice";
+import cartSlice from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     products: productsSlice,
+    cart: cartSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
