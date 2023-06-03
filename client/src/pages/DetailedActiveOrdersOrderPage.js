36979bc8be7710d29f204bc2863c86c0d77255b8
andrijaristic <andra.rs@outlook.com>
2023-06-03 08:21:40 +0200

    Added detailed active buyer orders page

---
+++ client/src/pages/DetailedActiveOrdersOrderPage.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getDetailedOrderAction,
+  clearDetailedOrder,
+} from "../store/ordersSlice";
+import { useParams } from "react-router-dom";
+import DetailedOrderList from "../components/DetailedOrder/DetailedOrderList";
+
+const DetailedActiveOrdersOrderPage = () => {
+  const dispatch = useDispatch();
+  const detailedOrder = useSelector((state) => state.orders.detailedOrder);
+  const params = useParams();
+  const id = params.orderId || "";
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearDetailedOrder());
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getDetailedOrderAction(id));
+  }, [dispatch, id]);
+
+  return <DetailedOrderList order={detailedOrder} admin={false} />;
+};
+
+export default DetailedActiveOrdersOrderPage;
