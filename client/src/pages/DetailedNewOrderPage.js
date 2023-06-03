7fb229414cff13125a98470a0ed82c40296c6c9d
andrijaristic <andra.rs@outlook.com>
2023-06-03 08:20:36 +0200

    Added detailed new seller order page

---
+++ client/src/pages/DetailedNewOrderPage.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getSellerDetailedOrderAction,
+  clearDetailedOrder,
+} from "../store/ordersSlice";
+import { useParams } from "react-router-dom";
+import DetailedOrderList from "../components/DetailedOrder/DetailedOrderList";
+
+const DetailedNewOrderPage = () => {
+  const dispatch = useDispatch();
+  const detailedOrder = useSelector((state) => state.orders.detailedOrder);
+  const params = useParams();
+  const id = params.orderId || "";
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearDetailedOrder());
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getSellerDetailedOrderAction(id));
+  }, [dispatch, id]);
+
+  return <DetailedOrderList order={detailedOrder} admin={false} />;
+};
+
+export default DetailedNewOrderPage;
